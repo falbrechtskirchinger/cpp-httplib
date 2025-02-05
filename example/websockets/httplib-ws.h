71a4eb9b0f074d0efcd052a133b3366db783460a
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2025-02-05 20:40:28 +0100

    Add httplib-ws.h

---
+++ example/websockets/httplib-ws.h
@@ -0,0 +1,45 @@
+//
+//  httplib-ws.h
+//
+//  Copyright (c) 2025 Florian Albrechtskirchinger. All rights reserved.
+//  MIT License
+//
+
+#ifndef CPPHTTPLIB_HTTPLIB_WS_H
+#define CPPHTTPLIB_HTTPLIB_WS_H
+
+/*
+ * Configuration
+ */
+
+#ifndef CPPHTTPLIB_OPENSSL_SUPPORT
+#error CPPHTTPLIB_OPENSSL_SUPPORT is required
+#endif
+
+/*
+ * Headers
+ */
+
+#include "httplib.h"
+
+/*
+ * Declaration
+ */
+
+namespace httplib {
+
+
+
+// ----------------------------------------------------------------------------
+
+/*
+ * Implementation that will be part of the .cc file if split into .h + .cc.
+ */
+
+
+
+// ----------------------------------------------------------------------------
+
+} // namespace httplib
+
+#endif // CPPHTTPLIB_HTTPLIB_WS_H
