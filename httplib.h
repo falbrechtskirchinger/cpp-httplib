190291966f9f1b2b912e0fb0effc05d8d7e727da
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2025-02-08 18:30:17 +0100

    Add "pipes" for all platforms

--- httplib.h
+++ httplib.h
@@ -3297,6 +3297,106 @@ inline bool is_socket_alive(socket_t sock) {
   return detail::read_socket(sock, &buf[0], sizeof(buf), MSG_PEEK) > 0;
 }
 
+inline bool pipe(socket_t fds[2]) {
+  fds[0] = fds[1] = INVALID_SOCKET;
+
+#if defined(__linux__) && defined(_GNU_SOURCE)
+  if (::pipe2(fds, O_CLOEXEC | O_NONBLOCK) < 0) { return false; }
+#elif !defined(_WIN32)
+  if (::pipe(fds) < 0) { return false; }
+  if (::fcntl(fds[0], F_SETFD, FD_CLOEXEC) < 0) { return false; }
+  if (::fcntl(fds[0], F_SETFL, O_NONBLOCK) < 0) { return false; }
+  if (::fcntl(fds[1], F_SETFD, FD_CLOEXEC) < 0) { return false; }
+  if (::fcntl(fds[1], F_SETFL, O_NONBLOCK) < 0) { return false; }
+#else
+  return false;
+#endif
+
+  return true;
+}
+
+void set_nonblocking(socket_t sock, bool nonblocking);
+
+inline bool socketpair_inet(socket_t socks[2]) {
+  socks[0] = socks[1] = INVALID_SOCKET;
+
+  socket_t lsock = INVALID_SOCKET, ssock = INVALID_SOCKET,
+           csock = INVALID_SOCKET;
+
+  auto se = scope_exit([&]() {
+    if (lsock != INVALID_SOCKET) { close_socket(lsock); }
+    if (ssock != INVALID_SOCKET) { close_socket(ssock); }
+    if (csock != INVALID_SOCKET) { close_socket(csock); }
+  });
+
+  lsock = ::socket(AF_INET, SOCK_STREAM, 0);
+  csock = ::socket(AF_INET, SOCK_STREAM, 0);
+  if (lsock == INVALID_SOCKET || csock == INVALID_SOCKET) { return false; }
+
+  int opt = 1;
+  if (::setsockopt(lsock, SOL_SOCKET,
+#if defined(_WIN32) || !defined(SO_REUSEPORT)
+                   SO_REUSEADDR,
+#else
+                   SO_REUSEPORT,
+#endif
+                   &opt, sizeof(opt))) {
+    return false;
+  }
+
+  struct sockaddr_in addr{};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = ::htonl(INADDR_LOOPBACK);
+  addr.sin_port = 0;
+
+  if (::bind(lsock, reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr))) {
+    return false;
+  }
+  if (::listen(lsock, 1)) { return false; }
+
+  socklen_t addrlen = sizeof(addr);
+  if (::getsockname(lsock, reinterpret_cast<struct sockaddr *>(&addr),
+                    &addrlen)) {
+    return false;
+  }
+  if (::connect(csock, reinterpret_cast<struct sockaddr *>(&addr),
+                sizeof(addr))) {
+    return false;
+  }
+  ssock = ::accept(lsock, NULL, NULL);
+
+  // Pedantic check against CVEs like CVE-2024-3219
+  auto auth_connection = [](socket_t sock, socket_t peer) -> bool {
+    struct sockaddr_in sock_addr, peer_addr;
+    socklen_t sock_addrlen = sizeof(sock_addr),
+              peer_addrlen = sizeof(peer_addr);
+
+    if (::getsockname(sock, reinterpret_cast<struct sockaddr *>(&sock_addr),
+                      &sock_addrlen)) {
+      return false;
+    }
+    if (::getpeername(peer, reinterpret_cast<struct sockaddr *>(&peer_addr),
+                      &peer_addrlen)) {
+      return false;
+    }
+
+    if (sock_addrlen != peer_addrlen) { return false; }
+    return std::memcmp(&sock_addr, &peer_addr, sock_addrlen) == 0;
+  };
+
+  if (!auth_connection(ssock, csock) || !auth_connection(csock, ssock)) {
+    return false;
+  }
+
+  set_nonblocking(ssock, true);
+  set_nonblocking(csock, true);
+  socks[0] = csock;
+  socks[1] = ssock;
+  ssock = csock = INVALID_SOCKET; // Prevent closing on scope exit
+
+  return true;
+}
+
 class SocketStream final : public Stream {
 public:
   SocketStream(socket_t sock, time_t read_timeout_sec, time_t read_timeout_usec,
